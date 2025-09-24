84c774b0589d1360aa5c6cbd6440d6744ddc5a2a
Andrij72 <akulmmm@gmeil.com>
2025-09-24 23:25:42 +0200

    build(order-service): update Dockerfile, Compose, and CI pipeline

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,1 +1,4 @@
 CREATE DATABASE IF NOT EXISTS order_service;
+CREATE USER 'usermysql'@'%' IDENTIFIED WITH mysql_native_password BY 'mysql';
+GRANT ALL PRIVILEGES ON order_service.* TO 'usermysql'@'%';
+FLUSH PRIVILEGES;
